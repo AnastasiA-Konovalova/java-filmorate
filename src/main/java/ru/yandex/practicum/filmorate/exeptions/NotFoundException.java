f2ac1cfd65c269739359a268cd68e58d6ab62a3f
AnastasiA-Konovalova <nastia90709a@yandex.ru>
2024-11-24 13:20:44 +0300

    fear: добавлены класс хранилище и сервис с соответствующей функциональностью

--- src/main/java/ru/yandex/practicum/filmorate/exeptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeptions/NotFoundException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.exeptions;
 
 public class NotFoundException extends RuntimeException {
 
-    public NotFoundException (String message) {
+    public NotFoundException(String message) {
         super(message);
     }
 }

e7cf7da377bfcba6f51b4c64d3b6df918152b3d3
AnastasiA-Konovalova <nastia90709a@yandex.ru>
2025-02-24 01:41:12 +0300

    feat: добавлена база данных

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
